9d71c6312a6b570415ee84b807cb4fc552643468
Suthakar Thiroshan <sthiroshan@gmail.com>
2025-12-08 21:36:22 +0530

    Working commit need to optimize it

--- plates.py
+++ plates.py
@@ -4,13 +4,9 @@
 # “No periods, spaces, or punctuation marks are allowed.”
 # Assume that any letters in the user’s input will be uppercase. 
 
-# #initial plan
-# 2 <= len(string) <= 6
-# slice the list [0:2] and check whether first two are letters or not
-# use in statement to check whether there are punctuation and spaces
-# check endwith method to check whether number is in end 
 
 def main():
+
     plate = input("Plate: ")
     if is_valid(plate): #is_valid going to return bool value
         print("Valid")
@@ -20,7 +16,7 @@ def main():
 
 def is_valid(s):
 
-    if length_check(s) and first_2_letter_check(s) and end_is_number_check(s) and no_punctuation_space_check(s):
+    if length_check(s) and first_2_letter_check(s) and number_middle_check(s) and no_punctuation_space_check(s): #I created function for all type of check
         return True
     else:
         return False
@@ -35,9 +31,9 @@ def length_check(plate):
 
 def first_2_letter_check(plate):
 
-    first_2_letter = plate[0:2] #slice first two value of plate string
+    first_2_letter = plate[0:2] # Slice first two value of plate string
 
-    if not first_2_letter.isdecimal(): #checks whether it's not numeric character 
+    if not first_2_letter.isdecimal(): # Checks whether it's not numeric character 
         return True # If it's not decimal, it's Alphabet ! (Note: punctuation check is checked in other function)
     else:
         return False 
@@ -45,33 +41,38 @@ def first_2_letter_check(plate):
 def no_punctuation_space_check(plate):
 
     check = True
-    punctuation = ["!", "#", "$", "%", "&" , "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";" ,"<" , "=", ">", "?", "@", "[", "]", "^", "`", "{", "|" ,"}", "~",'"'," "]
+    punctuation_space = ["!", "#", "$", "%", "&" , "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";" ,"<" , "=", ">", "?", "@", "[", "]", "^", "`", "{", "|" ,"}", "~",'"'," "]
     for i in plate:
-        if i in punctuation:
+        if i in punctuation_space:
             check = False
             
     if check: return True
     else: return False
 
-def end_is_number_check(plate):
-    # Iterate plate 
-    # check whether there is number
-    # if then check whether it's in last or not
+def number_middle_check(plate):
+    # Itereate each element in that created list and check whether it contain number or not
 
-    number = False
+    number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    for i in plate: # Checks whether there is number in inputed plate number
-        if i.isdecimal(): 
-            number = True 
+    plate_list = list(plate) # Make new  list 
+    sliced_plate_list = plate_list[2:-1] # Slice it using [2:-1], it doesnt include first, second and last element
 
-    if number: # Check whether number === True
-        if plate.endswith(0, 1, 2, 3, 4, 5, 6 ,7 ,8, 9): # check whet
-            return True
-        else: 
-            return False # Numbers are in middle or at start
-        
-    else:
-        return True # Returns True if there is no number , so there won't be numbers in middle
+    for char in sliced_plate_list: # Iterates for each element in the sliced list
+
+        if char in number: # Detects number
+                    
+            index_for_first_detected_number = plate_list.index(char) # Returns the index of the first occurrence of the detected number. 
+            remaining_list = plate_list[index_for_first_detected_number:]
+            remaining_string = "".join(remaining_list) # Joints the remaining list into string
+
+            if char == "0": return False # Number starts with 0
+            
+            if remaining_string.isdecimal(): #Return True or False by check if number isn't in the middle,  break the loop and function
+                return True # Number is not in middle
+            else: 
+                return False #Number is in middle
+
+    return True # Return True if there is no number in the middle 
 
 
 main()
